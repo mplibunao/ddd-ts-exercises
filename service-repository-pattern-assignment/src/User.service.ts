3c7984040ad719943b008d092fe5682f63522a6e
MP <markpaololibunao@gmail.com>
2022-10-28 06:45:07 +0800

    Makes service tests pass

--- service-repository-pattern-assignment/src/User.service.ts
+++ service-repository-pattern-assignment/src/User.service.ts
@@ -2,4 +2,14 @@ import { UserRequestDto } from './Database'
 import { UserRepository } from './User.repository'
 
 // NOTE: Make userRepo public and not private. This is required for the tests!
-export class UserService {}
+export class UserService {
+  public readonly userRepo: UserRepository
+
+  constructor() {
+    this.userRepo = new UserRepository()
+  }
+
+  public async createUser(user: UserRequestDto) {
+    return this.userRepo.createUser(user)
+  }
+}
