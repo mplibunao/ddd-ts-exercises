8e49472f89e1677b81d701daa10a1d9beef3dfdc
MP <markpaololibunao@gmail.com>
2022-10-28 06:38:41 +0800

    Makes tests passing

--- service-repository-pattern-assignment/src/User.repository.ts
+++ service-repository-pattern-assignment/src/User.repository.ts
@@ -1,4 +1,14 @@
-import Database, { UserRequestDto } from './Database'
+import Database, { IUser, UserRequestDto } from './Database'
 
 // NOTE: Make db public instead of private. This is required for the tests.
-export class UserRepository {}
+export class UserRepository {
+  public db: typeof Database
+
+  constructor() {
+    this.db = Database
+  }
+
+  public async createUser(user: UserRequestDto): Promise<IUser> {
+    return await this.db.create(user)
+  }
+}
