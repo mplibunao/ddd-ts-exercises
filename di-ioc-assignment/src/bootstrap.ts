ab8969866422e27397b5df41d03f1d69a0b8525e
MP <markpaololibunao@gmail.com>
2022-10-28 15:22:05 +0800

    Makes tests pass by copy pasting

--- di-ioc-assignment/src/bootstrap.ts
+++ di-ioc-assignment/src/bootstrap.ts
@@ -14,7 +14,25 @@ export class UserRepository {
   }
 }
 
-export class UserService {}
-export class UserController {}
+export class UserService {
+  constructor(
+    private _userRepository: UserRepository = container.get(UserRepository.name)
+  ) {}
+
+  public getUsers() {
+    return this._userRepository.getUsers()
+  }
+}
+export class UserController {
+  constructor(
+    private readonly _userService: UserService = container.get(UserService.name)
+  ) {}
+
+  public index() {
+    return this._userService.getUsers()
+  }
+}
 
 export const container = new Container()
+container.bind(UserRepository.name).to(UserRepository)
+container.bind(UserService.name).to(UserService)
